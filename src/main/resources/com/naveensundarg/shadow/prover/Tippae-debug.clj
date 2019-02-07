d6ccf3e919b65bc3aaab100a75d7ebfa9393b18c
John <Angelson1992@gmail.com>
2019-02-06 21:35:41 -0500

    Fixed some merging issues. Explored several paths of making multilayered Optionally branches and none of them were found usable. Added notes on thoughts and issues. Current thought process is to do work outside of the clj file to create new queries based on information form the old queries.

--- src/main/resources/com/naveensundarg/shadow/prover/Tippae-debug.clj
+++ src/main/resources/com/naveensundarg/shadow/prover/Tippae-debug.clj
@@ -211,13 +211,16 @@
 
 
 {
-:name Test-ValidityInheritance-with-2LayerChoiceList
+:name Test-ValidityInheritance-with-2LayerChoiceList-WIP
 :assumptions
   {
 
+;; A1 and its subsections explicitly state the definition of the various types of TwoByOneMult steps
+;; along with stating that it is fine to directly replace those steps with their broken down versions.
+
   A1   (forall (?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (=  (TwoByOneMult ?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
-              (+
+              (Addition
                   (MultiplicandOnesDigitTimesMultiplierOnesDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
                   (MultiplicandTensDigitTimesMultiplierOnesDigit ?MultiplicandTensDigit ?MultiplierOnesDigit)
               )
@@ -226,7 +229,7 @@
 
   A1-2 (forall (?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (= (TwoByOneMultErr1 ?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
-              (+
+              (Addition
                   (MultiplicandTensDigitTimesMultiplicandOnesDigit ?MultiplicandTensDigit ?MultiplicandOnesDigit)
                   (MultiplicandTensDigitTimesMultiplierOnesDigit ?MultiplicandTensDigit ?MultiplierOnesDigit)
               )
@@ -235,17 +238,19 @@
 
   A1-3 (forall (?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (= (TwoByOneMultErr2 ?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
-              (+
+              (Addition
                   (MultiplicandOnesDigitTimesMultiplierOnesDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
                   (MultiplicandTensDigitTimesMultiplicandOnesDigit ?MultiplicandTensDigit ?MultiplicandOnesDigit)
               )
           )
        )
 
+;; This is not currently being used.
+
   A2  (forall (?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (Define
               (MultiplicandOnesDigitTimesMultiplierOnesDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
-              (* ?MultiplicandOnesDigit ?MultiplierOnesDigit)
+              (Optionally (OneByOneMultAttempt ?MultiplicandOnesDigit ?MultiplierOnesDigit) ?Choice)
           )
       )
 
@@ -262,6 +267,11 @@
               (* ?MultiplicandTensDigit ?MultiplicandOnesDigit)
           )
       )
+
+;; A5-A8 are representing that validity of various possible steps one could take in the process of multiplying
+;; a two digit number by a one digit number. They are used to both state that a specific step is valid or
+;; invalid as an assertion and to state that it is fine to substitute to substitute the raw step with
+;; the explicit validity statement during reasoning.
 
   A5  (forall (?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (and
@@ -327,63 +337,70 @@
           )
       )
 
+;; A9 and it's subsections are a representation of how the validity of two part addition steps
+;; is built and maintained from it's parts.
+
   A9  (forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (+ (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Valid) )
+              (Addition (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Valid) )
               Valid
           )
       )
 
   A9-1(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (+ (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Valid) )
+              (Addition (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Valid) )
               Invalid
           )
       )
 
   A9-2(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (+ (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Invalid) )
+              (Addition (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Invalid) )
               Invalid
           )
       )
 
   A9-3(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (+ (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Invalid) )
+              (Addition (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Invalid) )
               Invalid
           )
       )
 
+;; A10 and it's subsections are a representation of how the validity of two part multiplication steps
+;; is built and maintained from it's parts.
+
   A10 (forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (* (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Valid) )
+              (Multiplication (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Valid) )
               Valid
           )
       )
 
   A10-1(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (* (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Valid) )
+              (Multiplication (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Valid) )
               Invalid
           )
       )
 
   A10-2(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (* (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Invalid) )
+              (Multiplication (Validity ?AddedStep1 Valid) (Validity ?AddedStep2 Invalid) )
               Invalid
           )
       )
 
   A10-3(forall (?AddedStep1 ?AddedStep2)
           (Validity
-              (* (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Invalid) )
+              (Multiplication (Validity ?AddedStep1 Invalid) (Validity ?AddedStep2 Invalid) )
               Invalid
           )
       )
 
-
+;; A11 through A13 are representations of various attempts at multiplying two digit numbers by one digit
+;; numbers. We'll call this idea an option tree.
 
   A11   (forall (?MultiplicandTensDigit ?MultiplicandOnesDigit ?MultiplierOnesDigit)
           (Optionally
@@ -406,16 +423,67 @@
           )
        )
 
+;; A14 through A17 are representations of various attempts at multiplying one digit numbers by one digit
+;; numbers. We'll call this idea an option tree.
+
+  A14   (forall (?MultiplierDigit ?MultiplicandDigit)
+          (Optionally
+              (OneByOneMultAttempt ?MultiplierDigit ?MultiplicandDigit)
+              (OneByOneMult ?MultiplierDigit ?MultiplicandDigit)
+          )
+       )
+
+  A15   (forall (?MultiplierDigit ?MultiplicandDigit)
+          (Optionally
+              (OneByOneMultAttempt ?MultiplierDigit ?MultiplicandDigit)
+              (OneByOneMultErr1 ?MultiplierDigit ?MultiplicandDigit)
+          )
+       )
+
+  A16   (forall (?MultiplierDigit ?MultiplicandDigit)
+          (Optionally
+              (OneByOneMultAttempt ?MultiplierDigit ?MultiplicandDigit)
+              (OneByOneMultErr2 ?MultiplierDigit ?MultiplicandDigit)
+          )
+       )
+
+  A17   (forall (?MultiplierDigit ?MultiplicandDigit)
+          (Optionally
+              (OneByOneMultAttempt ?MultiplierDigit ?MultiplicandDigit)
+              (OneByOneMultErr3 ?MultiplierDigit ?MultiplicandDigit)
+          )
+       )
+
+  A18  (forall (?Attempt ?Choice)
+          (=
+              (Answered (Optionally ?Attempt ?Choice) )
+              ?Choice
+          )
+       )
+
+
   }
 
- :answer-variables [?x]
+ :answer-variables [?x, ?foo, ?bar, ?day]
 
  :answers-expected ( )
+
+;; ABANDON: The notion of doing math in the snark. This is because we wouldn't have the lowest levels be unique.
+;; ABANDON: The notion of using the justification in further logic. It's difficult to access in addition to not holding the direct math.
+;; NOTE: The output will most likely be the lowest level of representation when I write their validity.
+;; QUESTION: Can we replace the intermediate validity ratings and how can I write the rules for those.
+;; NOTE: Keep in mind that all parts of the branching logic need to be unique identified to work.
+;; CONSIDER: Embracing the limitations we're running into with multilevel branching.
+;;      It's possible that creating a system that explicitly calls the lower levels gives up access to specifying their validity or simply assuming they are correct.
+;;      Current attempt is to build a similar problem based on the results and then run it on the results.
 
  :goal
         (and
             (Validity ?x valid)
-            (Optionally (TwoByOneMultAttempt 1 5 5) ?x)
+            (Optionally (TwoByOneMultAttempt 1 5 5) ?x) ;;This represents the questions being analysed and is not optional
+            (= ?x (addition ?foo ?bar))
+            (Define ?foo ?day)
+
         )
 
 }
